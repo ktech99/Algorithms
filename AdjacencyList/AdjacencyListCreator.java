2ad5805a3e79a7fd010997698d4a7aaee078a60d
Kartik Arora <33497301+ktech99@users.noreply.github.com>
2018-03-21 15:22:21 -0700

    Added comments/Removed redundency

--- AdjacencyList/AdjacencyListCreator.java
+++ AdjacencyList/AdjacencyListCreator.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class AdjacencyListCreator {
 
-  private int nodes;
-  private int edges;
-  private Map<Integer, Set<Integer>> adjList;
+  private int nodes; // Total Number of nodes
+  private int edges; // Total number of edges
+  private Map<Integer, Set<Integer>> adjList; // The Adjacency List
 
   public AdjacencyListCreator(int nodes, int edges) {
     this.nodes = nodes;
@@ -12,6 +12,7 @@ public class AdjacencyListCreator {
     adjList = new TreeMap<Integer, Set<Integer>>();
   }
 
+  // Adds nodes and their respective adjacent nodes in the list
   public void creator() {
     Scanner sc = new Scanner(System.in);
     for (int i = 1; i <= edges; i++) {
@@ -19,23 +20,23 @@ public class AdjacencyListCreator {
       int x = sc.nextInt();
       System.out.print("Enter node 2: ");
       int y = sc.nextInt();
-      if (adjList.containsKey(x)) {
-        adjList.get(x).add(y);
-      } else {
-        Set<Integer> connections = new TreeSet<Integer>();
-        connections.add(y);
-        adjList.put(x, connections);
-      }
-      if (adjList.containsKey(y)) {
-        adjList.get(y).add(x);
-      } else {
-        Set<Integer> connections = new TreeSet<Integer>();
-        connections.add(x);
-        adjList.put(y, connections);
-      }
+      adder(x, y);
+      adder(y, x);
     }
   }
 
+  // Adds node and its neighbout to the list
+  private void adder(int n1, int n2) {
+    if (adjList.containsKey(n1)) {
+      adjList.get(n1).add(n2);
+    } else {
+      Set<Integer> connections = new TreeSet<Integer>();
+      connections.add(n2);
+      adjList.put(n1, connections);
+    }
+  }
+
+  // Prints the adjacency list
   public void print() {
     System.out.println(adjList);
   }
