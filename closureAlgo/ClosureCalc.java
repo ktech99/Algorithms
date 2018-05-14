52205973c3c0a1709d01f4493208fa3304eb1d1d
Kartik Arora <kartikar@uw.edu>
2018-05-13 20:33:49 -0700

    regex values

--- closureAlgo/ClosureCalc.java
+++ closureAlgo/ClosureCalc.java
@@ -3,18 +3,30 @@ import java.util.*;
 public class ClosureCalc {
 
   private Map<String, String> lineMap;
+  private Map<String, String> lineMapToSet;
+  private Set<String> values;
 
   public ClosureCalc(List<String> lines) {
     if (lines.size() == 0) {
       throw new IllegalArgumentException("Empty file");
     }
     lineMap = new HashMap<String, String>();
+    lineMapToSet = new HashMap<String, String>();
     for (String line : lines) {
       String[] lineDiv = line.split("-");
       if (lineMap.containsKey(lineDiv[0])) {
         throw new IllegalArgumentException("Wrong file format");
       }
       lineMap.put(lineDiv[0], lineDiv[1]);
+    }
+    for (String key : lineMap.keySet()) {
+      values = new HashSet<>();
+      String[] valDiv = lineMap.get(key).split(",");
+      for (int i = 0; i < valDiv.length; i++) {
+        values.add(valDiv[i]);
+      }
+      values.add(key);
+      lineMapToSet.put(key, values);
     }
   }
 }
